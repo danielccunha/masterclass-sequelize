8ea64fa14168ee03eaa43366891d4a547be0388b
Daniel Cunha <danielcunha54@gmail.com>
2020-05-31 22:54:13 -0300

    Setup environment variables

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+require('dotenv/config');
 const express = require('express');
 const chalk = require('chalk');
 
 const app = express();
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${chalk.green(port)}`);
